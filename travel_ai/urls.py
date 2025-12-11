2f7a9e4f1902383447de3e55daaf861d7f57a7b4
yashfeenraavi09 <yashfeenraavi1@gmail.com>
2025-12-11 22:04:31 +0530

    View changed

--- travel_ai/urls.py
+++ travel_ai/urls.py
@@ -1,7 +1,13 @@
+# travel_ai/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def root_view(request):
+    return JsonResponse({"message": "Travel AI Backend is live!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('itinerary.urls')),
+    path('', root_view), 
 ]
